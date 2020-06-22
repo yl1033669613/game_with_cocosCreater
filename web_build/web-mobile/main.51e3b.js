f35e8343ef79837392c8a54451b5f714b196e4c4
unknown <1033669613@qq.com>
2020-06-22 16:23:44 +0800

    发布web版

--- web_build/web-mobile/main.51e3b.js
+++ web_build/web-mobile/main.51e3b.js
@@ -91,6 +91,7 @@ window.boot = function () {
             }
             cc.view.enableAutoFullScreen([
                 cc.sys.BROWSER_TYPE_BAIDU,
+                cc.sys.BROWSER_TYPE_BAIDU_APP,
                 cc.sys.BROWSER_TYPE_WECHAT,
                 cc.sys.BROWSER_TYPE_MOBILE_QQ,
                 cc.sys.BROWSER_TYPE_MIUI,
@@ -140,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.cda81.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f6aa7.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -178,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.1d5db.js');
+        require('src/settings.066fd.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -186,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.1d5db.js');
+        require('src/settings.066fd.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
